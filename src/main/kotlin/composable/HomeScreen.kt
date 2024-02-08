e3763d43268bddd6cd98a95074027b7951645b84
Georgios Karanikolas <gkaranikolas@arx.net>
2024-02-08 12:19:15 +0200

    Add empty HomeScreen

---
+++ src/main/kotlin/composable/HomeScreen.kt
@@ -0,0 +1,25 @@
+package composable
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun HomeScreen(
+
+) {
+
+}
+
+@Composable
+private fun HomeContent() {
+
+}
+
+@Preview
+@Composable
+private fun HomeContentPreview() {
+    MaterialTheme {
+        HomeContent()
+    }
+}
